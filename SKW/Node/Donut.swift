ae3e4f63545434e810613264a64d3f54c350ec93
Tony <sirica.tony@gmail.com>
2018-03-13 21:57:10 +0100

    deltaTime implemented

--- SKW/Node/Donut.swift
+++ SKW/Node/Donut.swift
@@ -28,6 +28,7 @@ class Donut: SKSpriteNode {
     var currForce = Vector2(x: 0, y: 0)
     
     var gameScene: SKScene?
+    
     init() {
         let rand = Int(arc4random_uniform(UInt32(GameManager.shared.allDonutsTextures.count)))
         super.init(texture: GameManager.shared.allDonutsTextures[rand], color: .clear, size: SpriteSize.donutBig)
@@ -79,13 +80,16 @@ class Donut: SKSpriteNode {
         
         let gravityVector = Vector2(x: GameManager.shared.gravity.x, y: GameManager.shared.gravity.y)
         var positionAsVector = Vector2(x: position.x, y: position.y)
-        if position.x < (hitBox?.r)! {xParameter = abs(xParameter!)} else if position.x > ((super.scene?.frame.width)! - (hitBox?.r)!) {xParameter = -(abs(xParameter!))}
+        if position.x < (hitBox?.r)! {xParameter = abs(xParameter!)} else if position.x > ((gameScene?.frame.width)! - (hitBox?.r)!) {xParameter = -(abs(xParameter!))}
         
         currForce.y += gravityVector.y * velocity * CGFloat(deltaTime)
-        positionAsVector.y += currForce.y
+        positionAsVector.y += currForce.y * velocity * CGFloat(deltaTime)
         positionAsVector.x = positionAsVector.x + (xParameter! * velocity * CGFloat(deltaTime))
 
-        if positionAsVector.y <= GameManager.shared.groundY {currForce.y = reflectParameter!}
+        if positionAsVector.y <= GameManager.shared.groundY {
+            currForce.y = reflectParameter!
+            positionAsVector.y = GameManager.shared.groundY
+        }
         
         position = CGPoint(x: positionAsVector.x, y: positionAsVector.y)
         updateHitBox()
